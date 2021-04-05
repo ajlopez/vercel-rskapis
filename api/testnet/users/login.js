7d9c21114a50748f013aa77b1e0a64ea14e2f2c5
ajlopez <webmaster@ajlopez.com>
2021-04-04 23:22:02 -0300

    Enabling cors in login

--- api/testnet/users/login.js
+++ api/testnet/users/login.js
@@ -6,7 +6,26 @@ const contracts = require('../../../lib/testnet/contracts');
 
 users.useEthUtil(require('ethereumjs-util'));
 
-module.exports = (req, res) => {
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
+}
+
+const handler = (req, res) => {
+    console.dir(req.headers);
+    
     if (!req.body || !req.body.username || !req.body.password) {
        res.json({});
        return;
@@ -35,3 +54,5 @@ module.exports = (req, res) => {
        })
         .catch(err => res.json({ error: err }));
 }
+
+module.exports = allowCors(handler)
