18a01270d0850b9afeb0ff3cf629bb5986850f80
ajlopez <webmaster@ajlopez.com>
2021-04-17 14:35:02 -0300

    Enabling cors in register

--- api/testnet/users/register.js
+++ api/testnet/users/register.js
@@ -19,7 +19,24 @@ const registryOwner = {
 //console.dir(faucetOwner);
 //console.dir(registryOwner);
 
-module.exports = (req, res) => {
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
+}
+
+const handler = (req, res) => {
     if (!req.body || !req.body.username || !req.body.password) {
        res.json({});
        return;
@@ -50,4 +67,7 @@ module.exports = (req, res) => {
                 .catch(err => res.json({ error: err }));
        })
         .catch(err => res.json({ error: err }));
-}
+};
+
+module.exports = allowCors(handler);
+
